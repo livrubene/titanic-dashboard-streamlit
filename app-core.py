4acfa311a4f2cb0164efd45725e5bcd7f2c8a882
Liv <148467076+livrubene@users.noreply.github.com>
2025-05-19 12:15:44 +0100

    app-core.py

--- app-core.py
+++ app-core.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
-
-plt.style.use('ggplot')
 
 # Load data
 df = pd.read_csv("Titanic-Dataset.csv")
